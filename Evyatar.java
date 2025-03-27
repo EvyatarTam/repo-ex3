51771bc5acc22d9203e2a770a7ef8274326aa940
evyat <evyatar.tam98@gmail.com>
2025-03-27 21:48:57 +0200

    ex3 two new files

---
+++ Evyatar.java
@@ -0,0 +1,8 @@
+public class Evyatar{
+	public static void main(String[] args){
+		String myName = "Evyatar Tam";
+		for(int i=0;i<=myName.length();i++){
+			System.out.println(myName.substring(0, i));
+		}
+	}
+}
