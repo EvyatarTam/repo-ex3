fed689f6ccb44cebd11e52dcaa198d1602c8a368
evyat <evyatar.tam98@gmail.com>
2025-03-27 22:13:36 +0200

    updated Evyatar.java file

--- Evyatar.java
+++ Evyatar.java
@@ -1,6 +1,6 @@
 public class Evyatar{
 	public static void main(String[] args){
-		String myName = "Evyatar Tam";
+		String myName = "Tam Evyatar";
 		for(int i=0;i<=myName.length();i++){
 			System.out.println(myName.substring(0, i));
 		}
